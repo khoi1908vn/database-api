cd8b5576a76c4ee36de4a577e71ed3f746e45ffd
khoi1908vn <khoimle7@gmail.com>
2023-04-02 16:33:01 +0700

    system files

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ if [ -n "$NO_GIT_AUTOMATION" ] && [ "$NO_GIT_AUTOMATION" = "YES" ]; then
   git pull
   git reset --hard origin/main
 fi
-if [ -n "$NO_PACKAGE_INSTALLED" ] && [ "$NO_PACKAGE_INSTALLED" = "YES" ]; then
+if [ -n "$NO_PIP_INSTALLED" ] && [ "$NO_PIP_INSTALLED" = "YES" ]; then
   apt-get update && apt-get install -y python3-pip
 fi
 python -m pip install -r requirements.txt
