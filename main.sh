fcb588a86b69a787e18b4870b12c917e97b8a1d1
khoi1908vn <khoimle7@gmail.com>
2023-04-04 08:40:46 +0700

    exclusive support for Replit

--- main.sh
+++ main.sh
@@ -1,11 +1,19 @@
-if [ -n "$NO_GIT_AUTOMATION" ] && [ "$NO_GIT_AUTOMATION" = "YES" ]; then
-  git pull
-  git reset --hard origin/main
+if [ "$NO_GIT_AUTOMATION" = "YES" ]; then
+    echo ========================================== Updating git ==========================================
+    git pull
+    git reset --hard origin/main
 fi
-if [ -n "$NO_PIP_INSTALLED" ] && [ "$NO_PIP_INSTALLED" = "YES" ]; then
-  apt-get update && apt-get install -y python3-pip
+
+if [ "$NO_PIP_INSTALLED" -= "YES" ]; then
+    echo ========================================== Installing dependencies ==========================================
+    apt-get update && apt-get install -y python3-pip
 fi
-python -m pip install -r requirements.txt
+
+if [ "$IS_REPLIT" != "YES" ]; then
+    python -m pip install -r requirements.txt
+fi
+
+echo ========================================== Starting ==========================================
 uvicorn main:app --reload --port 8080 --host 0.0.0.0
 
 
